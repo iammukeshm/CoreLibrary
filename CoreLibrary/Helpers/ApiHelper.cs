8efa3dd399414a206e275e7eb7a1c928619dddde
Mukesh Murugan <iammukeshm@gmail.com>
2020-04-11 11:38:10 +0530

    initial commit

--- CoreLibrary/Helpers/ApiHelper.cs
+++ CoreLibrary/Helpers/ApiHelper.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace CoreLibrary.Helpers
 {
     public class ApiHelper
     {
+        public HttpClient ApiClient { get; set; }
     }
 }
